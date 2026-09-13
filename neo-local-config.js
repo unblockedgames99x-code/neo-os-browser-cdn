7ec33897bfe3ac1273293888855984d98a097ae4
unblockedgames99x-code <unblockedgames99x-code@users.noreply.github.com>
2026-09-13 09:20:13 -0500

    Optimize desktop responsiveness and restore complete assets

--- neo-local-config.js
+++ neo-local-config.js
@@ -7,7 +7,7 @@
   var base = new URL(configuredAssetBase, document.currentScript.src);
   var previewBase = new URL("http://127.0.0.1:3092/neo-os/");
   var isSitesHost = /(?:^|\.)chatgpt\.site$/i.test(window.location.hostname);
-  var hostedBrowserRoot = new URL("https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@cfe5196674de64378442dfb3aa10f135a3d049cb/nextnode-browser/");
+  var hostedBrowserRoot = new URL("https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@a5fdf700ee788acea0af7c35711ba0f6be425ff8/nextnode-browser/");
   window.NEO_LOCAL_CONFIG = Object.freeze({
     enabled: true,
     externalIntegrations: false,
